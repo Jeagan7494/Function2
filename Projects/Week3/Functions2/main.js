19064685121c620c34231596902d264b39e00eaa
Student <student@RU-104.local>
2015-09-15 16:42:10 -0600

    end of Tuesday

---
+++ Projects/Week3/Functions2/main.js
@@ -0,0 +1,53 @@
+// var getName = function(obj) {
+// 	return obj.name
+// }
+// console.log(getName({ name : 'Luisa', age : 25}))
+
+
+// var totalLetters = function(array) {
+// arrayLength = array.length
+// 	var totalLength = 0
+// 	for(i = 0; i < arrayLength; i++) {
+// 		totalLength += array[i].length
+		
+// 	}
+// 	return totalLength
+// }
+// console.log(totalLetters(['javascript', 'is', 'awesome']))
+// console.log(totalLetters(['what', 'happened', 'to', 'my', 'function']))
+
+// var keyValue = function(arg1, arg2) {
+// var	newObject = {}
+// 		newObject[arg1] = arg2;
+// 	return newObject
+
+// }
+// console.log(keyValue('city', 'Denver'))
+
+// //=================================================//
+// var negativeIndex = function(array, number) {
+// 	for (i = 0; i = array.indexOf(-2); i++) {
+		
+// 	}
+// 	return negativeIndex
+// }
+// console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2))
+// //=================================================//
+
+// var removeM = function(str) {
+// 		var noMs = str.replace(/m/g, '')	
+// 		return noMs
+// }		
+// 		console.log(removeM('family'))
+// 		console.log(removeM('memory'))
+
+var printObject = function(['key', 'value']) {
+	for (var i = 0; i < printObject.length; i++) {
+		console.log(printObject[i])
+	};
+	console.log()
+}
+console.log(printObject({ a: 10, b: 20, c: 30 }))
+
+
+
